8b563597aa490e9ca0d1eb1644b2539ade31a100
philgrayphilgray <thephilgray@gmail.com>
2018-02-28 12:19:37 -0500

    Feature test and implement create page.

---
+++ test/features/user-visiting-create-page-test.js
@@ -0,0 +1,13 @@
+const { assert } = require("chai");
+
+describe("when the user visits the `videos/create` page", () => {
+  describe("and submits the form with a title and description", () => {
+    const title = "Magnetic Sound Effects";
+    const description = "Collection of interesting magnetic sounds";
+    browser.url("/videos/create.html");
+    browser.setValue("form input[name=title]", title);
+    browser.setValue("form textarea", description);
+    browser.click(".submit-button");
+    assert.include(browser.getText("body"), "Videos");
+  });
+});
