85eff738225a6362b66c7e194935fbdb050f16a2
philgrayphilgray <thephilgray@gmail.com>
2018-02-28 13:01:45 -0500

    Add videosRoute to app.

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
-const path = require('path');
-const express = require('express');
-const expressHandlebars = require('express-handlebars');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
+const path = require("path");
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+
+const videosRoute = require("./routes/videos");
 
 const app = express();
 
 // View engine setup
-app.engine('handlebars', expressHandlebars({defaultLayout: 'app'}));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", expressHandlebars({ defaultLayout: "app" }));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "handlebars");
 
-if (process.env.NODE_ENV !== 'test') {
-  app.use(logger('dev'));
+if (process.env.NODE_ENV !== "test") {
+  app.use(logger("dev"));
 }
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", videosRoute);
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const err = new Error('Not Found');
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -28,11 +32,11 @@ app.use(function(req, res, next) {
 app.use((err, req, res, next) => {
   // Set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // Render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
