d011c90b421026505b10730377f50afdb734be7b
philgrayphilgray <thephilgray@gmail.com>
2018-03-02 11:02:37 -0500

    Refactor to test render of videos/show.

--- test/routes/create-test.js
+++ test/routes/create-test.js
@@ -1,11 +1,29 @@
 const { assert } = require('chai');
 const request = require('supertest');
+const { jsdom } = require('jsdom');
 
 const app = require('../../app');
 const Video = require('../../models/video');
-const { mongoose, databaseUrl, options } = require('../../database');
+const {
+  connectDatabase,
+  disconnectDatabase
+} = require('../database-utilities');
+
+const parseTextFromHTML = (htmlAsString, selector) => {
+  const selectedElement = jsdom(htmlAsString).querySelector(selector);
+  if (selectedElement !== null) {
+    return selectedElement.textContent;
+  } else {
+    throw new Error(
+      `No element with selector ${selector} found in HTML string.`
+    );
+  }
+};
 
 describe('Server path: `/videos`', () => {
+  beforeEach(connectDatabase);
+  afterEach(disconnectDatabase);
+
   describe('POST', () => {
     it('responds to new video creation with the status code 201', async () => {
       const response = await request(app)
@@ -15,15 +33,6 @@ describe('Server path: `/videos`', () => {
     });
   });
   describe('when a video is submitted to the server', () => {
-    beforeEach(async () => {
-      await mongoose.connect(databaseUrl, options);
-      await mongoose.connection.db.dropDatabase();
-    });
-
-    afterEach(async () => {
-      await mongoose.disconnect();
-    });
-
     const sampleData = {
       title: 'Magnetic Sound Effects',
       description: 'Collection of interesting magnetic sounds'
@@ -35,8 +44,13 @@ describe('Server path: `/videos`', () => {
         .type('form')
         .send(sampleData);
 
-      const video = await Video.findOne({ sampleData });
-      assert.isOk(video);
+      const video = await Video.findOne({});
+      assert.equal(video.title, sampleData.title);
+      assert.equal(parseTextFromHTML(response.text, 'h2'), sampleData.title);
+      assert.equal(
+        parseTextFromHTML(response.text, 'p'),
+        sampleData.description
+      );
     });
   });
 });
