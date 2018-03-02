db32e12185a85e61cc2bbc1b81979a03d91fab87
philgrayphilgray <thephilgray@gmail.com>
2018-03-02 09:46:20 -0500

    Test that video details are added to the db after submitting.

--- test/routes/create-test.js
+++ test/routes/create-test.js
@@ -1,14 +1,45 @@
-const { assert } = require("chai");
-const request = require("supertest");
-const app = require("../../app");
+const { assert } = require('chai');
+const request = require('supertest');
 
-describe("Server path: `/videos`", () => {
-  describe("POST", () => {
-    it("responds to new video creation with the status code 201", async () => {
+const app = require('../../app');
+const Video = require('../../models/video');
+const {
+  connectDatabase,
+  disconnectDatabase
+} = require('../database-utiliteis');
+
+describe('Server path: `/videos`', () => {
+  describe('POST', () => {
+    it('responds to new video creation with the status code 201', async () => {
       const response = await request(app)
-        .post("/videos")
-        .type("form");
+        .post('/videos')
+        .type('form');
       assert.equal(response.status, 201);
+    });
+  });
+  describe('when a video is submitted to the server', () => {
+    beforeEach(async () => {
+      await mongoose.connect(databaseUrl, options);
+      await mongoose.connection.db.dropDatabase();
+    });
+
+    afterEach(async () => {
+      await mongoose.disconnect();
+    });
+
+    const sampleData = {
+      title: 'Magnetic Sound Effects',
+      description: 'Collection of interesting magnetic sounds'
+    };
+
+    it('submits a video with a title and description', async () => {
+      const response = await request(app)
+        .post('/videos')
+        .type('form')
+        .send(sampleData);
+
+      const video = await Video.findOne({ sampleData });
+      assert.isOk(video);
     });
   });
 });
