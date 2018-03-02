9410bee45b1010947bc28d573704457cd33c794f
philgrayphilgray <thephilgray@gmail.com>
2018-03-02 10:02:21 -0500

    Test and implement String schema type for title and description.

--- models/video.js
+++ models/video.js
@@ -1,9 +1,14 @@
-const {mongoose} = require('../database');
+const { mongoose } = require('../database');
 
 const Video = mongoose.model(
   'Video',
   mongoose.Schema({
-    
+    title: {
+      type: String
+    },
+    description: {
+      type: String
+    }
   })
 );
 
--- test/models/video-test.js
+++ test/models/video-test.js
@@ -1,5 +1,7 @@
-const {assert} = require('chai');
-const {mongoose, databaseUrl, options} = require('../../database');
+const { assert } = require('chai');
+const { mongoose, databaseUrl, options } = require('../../database');
+
+const Video = require('../../models/video');
 
 async function connectDatabase() {
   await mongoose.connect(databaseUrl, options);
@@ -10,7 +12,25 @@ async function disconnectDatabase() {
   await mongoose.disconnect();
 }
 
+describe('#title', () => {
+  it('is a string', () => {
+    const titleAsInt = 3;
+    const video = new Video({ title: titleAsInt });
+
+    assert.strictEqual(video.title, titleAsInt.toString());
+  });
+});
+
+describe('#description', () => {
+  it('is a string', () => {
+    const descriptionAsInt = 5000;
+    const video = new Video({ description: descriptionAsInt });
+
+    assert.strictEqual(video.description, descriptionAsInt.toString());
+  });
+});
+
 module.exports = {
   connectDatabase,
-  disconnectDatabase,
-}
+  disconnectDatabase
+};
