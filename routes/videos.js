baafba2b58d2f39ab922302a62ec2555ab078b58
philgrayphilgray <thephilgray@gmail.com>
2018-03-02 10:03:28 -0500

    Create video item in POST request.

--- routes/videos.js
+++ routes/videos.js
@@ -1,7 +1,10 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const Video = require('../models/video');
 
-router.post("/videos", (req, res, next) => {
-  res.status(201).render("videos");
+router.post('/videos', async (req, res, next) => {
+  const { title, description } = req.body;
+  await Video.create({ title, description });
+  res.status(201).render('videos');
 });
 
 module.exports = router;
