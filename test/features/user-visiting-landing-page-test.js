1ec3cbe97ff7121a3a18d6353fd1aab0a94db49e
philgrayphilgray <thephilgray@gmail.com>
2018-03-02 09:27:07 -0500

    Improve it block description.

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,15 +1,15 @@
-const { assert } = require("chai");
+const { assert } = require('chai');
 
-describe("when a user visits the landing page with no existing videos", () => {
-  it("renders an empty `videos-container` element", () => {
-    browser.url("/");
+describe('when a user visits the landing page with no existing videos', () => {
+  it('renders an empty `videos-container` element', () => {
+    browser.url('/');
 
-    assert.equal(browser.getText("#videos-container"), "");
+    assert.equal(browser.getText('#videos-container'), '');
   });
 
-  it("renders a link to `videos/create.html`", () => {
-    browser.url("/");
-    browser.click(".add-video-button");
-    assert.include(browser.getText("body"), "Save a video");
+  it('renders a link that allows users to navigate to `videos/create.html`', () => {
+    browser.url('/');
+    browser.click('.add-video-button');
+    assert.include(browser.getText('body'), 'Save a video');
   });
 });
