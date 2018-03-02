fbae5cf2c123be88cfa094987c97b694f8835973
philgrayphilgray <thephilgray@gmail.com>
2018-03-02 10:22:11 -0500

    Refactor database code into utilities module.

--- test/models/video-test.js
+++ test/models/video-test.js
@@ -1,36 +1,30 @@
 const { assert } = require('chai');
-const { mongoose, databaseUrl, options } = require('../../database');
+const {
+  connectDatabase,
+  disconnectDatabase
+} = require('../database-utilities');
 
 const Video = require('../../models/video');
 
-async function connectDatabase() {
-  await mongoose.connect(databaseUrl, options);
-  await mongoose.connection.db.dropDatabase();
-}
+describe('Video model', () => {
+  beforeEach(connectDatabase);
+  afterEach(disconnectDatabase);
 
-async function disconnectDatabase() {
-  await mongoose.disconnect();
-}
+  describe('#title', () => {
+    it('is a string', () => {
+      const titleAsInt = 3;
+      const video = new Video({ title: titleAsInt });
 
-describe('#title', () => {
-  it('is a string', () => {
-    const titleAsInt = 3;
-    const video = new Video({ title: titleAsInt });
+      assert.strictEqual(video.title, titleAsInt.toString());
+    });
+  });
 
-    assert.strictEqual(video.title, titleAsInt.toString());
+  describe('#description', () => {
+    it('is a string', () => {
+      const descriptionAsInt = 5000;
+      const video = new Video({ description: descriptionAsInt });
+
+      assert.strictEqual(video.description, descriptionAsInt.toString());
+    });
   });
 });
-
-describe('#description', () => {
-  it('is a string', () => {
-    const descriptionAsInt = 5000;
-    const video = new Video({ description: descriptionAsInt });
-
-    assert.strictEqual(video.description, descriptionAsInt.toString());
-  });
-});
-
-module.exports = {
-  connectDatabase,
-  disconnectDatabase
-};
