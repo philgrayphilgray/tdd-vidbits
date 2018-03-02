a6e115d69c2903f46322716eeaa075c68af7fc46
philgrayphilgray <thephilgray@gmail.com>
2018-03-02 09:34:29 -0500

    Add it block for better runner output and test for data submitted.

--- test/features/user-visiting-create-page-test.js
+++ test/features/user-visiting-create-page-test.js
@@ -1,13 +1,16 @@
-const { assert } = require("chai");
+const { assert } = require('chai');
 
-describe("when the user visits the `videos/create` page", () => {
-  describe("and submits the form with a title and description", () => {
-    const title = "Magnetic Sound Effects";
-    const description = "Collection of interesting magnetic sounds";
-    browser.url("/videos/create.html");
-    browser.setValue("form input[name=title]", title);
-    browser.setValue("form textarea", description);
-    browser.click(".submit-button");
-    assert.include(browser.getText(".contents-container"), "Videos");
+describe('when the user visits the `videos/create` page', () => {
+  describe('and submits the form with a title and description', () => {
+    const title = 'Magnetic Sound Effects';
+    const description = 'Collection of interesting magnetic sounds';
+    it('it returns the user to the landing page which contains the newly submitted information', () => {
+      browser.url('/videos/create.html');
+      browser.setValue('form input[name=title]', title);
+      browser.setValue('form textarea', description);
+      browser.click('.submit-button');
+      assert.include(browser.getText('.contents-container'), title);
+      // assert.include(browser.getText('.contents-container'), description);
+    });
   });
 });
