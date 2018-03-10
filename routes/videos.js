165d6ca62b44dba7285a5f4450ea1534855476cc
philgrayphilgray <thephilgray@gmail.com>
2018-03-10 09:17:29 -0500

    Fix implementation errors, ensure videos are saved and rendered.

--- routes/videos.js
+++ routes/videos.js
@@ -1,18 +1,20 @@
 const router = require('express').Router();
 const Video = require('../models/video');
 
-router.get('/', (req, res) => {
-  res.render('index');
-});
-
 router.get('/videos/create', (req, res) => {
   res.render('videos/create');
 });
 
 router.post('/videos', async (req, res, next) => {
   const { title, description } = req.body;
-  const video = await Video.create({ title, description });
+  const video = await new Video({ title, description });
+  await video.save();
   res.status(201).render('videos/show', { video });
+});
+
+router.get('/', async (req, res, next) => {
+  const videos = await Video.find({});
+  res.status(201).render('index', { videos });
 });
 
 module.exports = router;
