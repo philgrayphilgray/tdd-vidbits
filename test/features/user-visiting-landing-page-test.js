3feeb433a524fbc55f497888d58f32765c14b81f
philgrayphilgray <thephilgray@gmail.com>
2018-03-11 09:38:43 -0400

    Add dynamic links to `videos/:videoId` from landing page.

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -42,4 +42,19 @@ describe('when a user visits the landing page with an exiting video', () => {
     browser.url('/');
     assert.equal(browser.getAttribute('iframe', 'src'), url);
   });
+
+  it('renders a link to the video details page at `videos/:videoId`', () => {
+    const title = 'Magnetic Sound Effects';
+    const description = 'Collection of interesting magnetic sounds';
+    const url = 'https://www.youtube.com/watch?v=Du1a_dgGoXc';
+
+    browser.url('/videos/create');
+    browser.setValue('form input[name=title]', title);
+    browser.setValue('form textarea', description);
+    browser.setValue('form input[name=url]', url);
+    browser.click('.submit-button');
+    browser.url('/');
+    browser.click('.video-title a');
+    assert.include(browser.getText('.contents-container'), description);
+  });
 });
